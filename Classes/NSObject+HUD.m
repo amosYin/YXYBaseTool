43b4d615dfa3d13e828c813246c4faa150be6153
Amos <1453254169@qq.com>
2018-11-30 19:15:39 +0800

    no message

--- Classes/NSObject+HUD.m
+++ Classes/NSObject+HUD.m
@@ -10,6 +10,25 @@
 
 @implementation NSObject (HUD)
 
+#pragma mark - 加载进度动画
+- (void)progressViewStart
+{
+    
+}
+#pragma mark - 添加在不同位置的进度动画
+- (void)progressViewStartInView:(UIView *)view
+{
+    
+}
+#pragma mark - 移除进度动画
+- (void)progressViewFinish
+{
+    
+}
 
+- (void)showAlertWithTitle:(NSString *)title{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title message:nil delegate:nil cancelButtonTitle:@"我知道了" otherButtonTitles: nil];
+    [alertView show];
+}
 
 @end
